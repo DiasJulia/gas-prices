1d9e8d586deb595c6515b2ca70ae5e704b8447b8
Júlia Dias <jedl@cin.ufpe.br>
2023-08-21 23:12:54 -0300

    feat: initial brasil map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import * as d3 from 'd3';
+
+import * as topojson from 'topojson-client';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent {
+  ngOnInit() {
+    const svg = d3.select('svg');
+
+    const projection = d3.geoMercator().scale(500).translate([900, 100]);
+
+    let path = d3.geoPath().projection(projection);
+
+    d3.json('assets/br-states.json').then((data: any) => {
+      console.log(data);
+      var states = topojson.feature(data, data.objects.states);
+
+      const g = svg.append('g').attr('fill', '#000');
+
+      g.append('path').datum(states).attr('d', path).attr('class', 'state');
+      console.log(states);
+    });
+  }
+}
